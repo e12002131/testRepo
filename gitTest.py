6feb91585f9f99469ca3181030b44cbfee601bb9
e12002131 <e12002131@student.tuwien.ac.at>
2024-03-17 19:56:19 +0100

    update

--- gitTest.py
+++ gitTest.py
@@ -1,9 +1,6 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[6]:
-
-
 import time
 from datetime import datetime
 import sys
@@ -12,10 +9,3 @@ while True:
     print(datetime.now(), flush=True, end="")
     print('\r', flush=True, end="")
     time.sleep(0.015)
-
-
-# In[ ]:
-
-
-
-
